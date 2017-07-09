19fb86cfc203400391fdb911091c834cd4ea2249
nanna_dk <nanna@e-nanna.dk>
2017-07-09 15:05:28 +0200

    Updated files

--- includes/insert/inserting.php
+++ includes/insert/inserting.php
@@ -2,20 +2,20 @@
 /*
 * Function to insert info into db: thisfile.php?id=x
 */
-include (realpath(__DIR__ . '/../db.php'));
 
-$content = mysqli_real_escape_string($conn, "String containing 'quotes'");
-$id = mysqli_real_escape_string($conn, intval($_REQUEST["id"]));
+    include (realpath(__DIR__ . '/../db.php'));
+    $content = mysqli_real_escape_string($conn, "String containing 'quotes'");
+    $id = mysqli_real_escape_string($conn, intval($_REQUEST["id"]));
+    $sql = "UPDATE " . $DBtable . " SET content = '$content' WHERE id = '$id'";
+    $rs=$conn->query($sql);
 
-$sql = "UPDATE " . $DBtable . " SET content = '$content' WHERE id = '$id'";
-$rs = $conn->query($sql);
+    if ($rs === TRUE) {
+      echo "Record updated successfully";
+    } else {
+        echo "Error updating record: " . $conn->error;
+    }
 
-if ($rs === false) {
-  trigger_error('Wrong SQL: ' . $sql . ' Error: ' . $conn->error, E_USER_ERROR);
-}
-else {
-  echo "Record updated successfully";
-}
-// Close connection
-$conn->close();
+    // Close connection
+    $conn->close();
+
 ?>
