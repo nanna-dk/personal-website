9c2c3b491fca2531cb45b078ef4782e014fce240
Nanna Ellegaard <nanna@e-nanna.dk>
2018-02-02 12:06:37 +0100

    Rating

--- includes/downloads/allAssignments.php
+++ includes/downloads/allAssignments.php
@@ -1,36 +1,53 @@
 <?php
 // Query the database
-include (realpath(__DIR__ . '/../db.php'));
+include(realpath(__DIR__ . '/../db.php'));
 
-  $sql = "SELECT * FROM " . $DBtable . " ORDER BY dates DESC";
-  $stmt = $pdo->prepare($sql);
-  $stmt->execute();
-  //set counter variable
-  $counter = 1;
-    if ($stmt->rowCount() > 0) {
-        $result = $stmt->fetchAll();
-        foreach($result as $row) {
-            $id = $row['id'];
-            $title = $row['title'];
-            $desc = $row['description'];
-            $content = $row['content'];
-            $url = $row['url'];
-            $clicks = number_format($row['clicks'], 0, '', '.');
-            $dates = (date('d.m.Y', strtotime($row['dates'])));
-            echo '<div class="card">';
-            echo '<h4 class="card-header"><span class="count">' . $counter . '.</span> <a href="includes/downloads/downloads.php?id=' . $id . '" target="_blank" rel="noopener" title="Download">' . $title . '</a></h4>';
-            echo '<div class="card-block"><p class="card-text">' . $desc . '</p></div>';
-            echo '<div class="card-footer"><div class="footer-left">Oprettet: ' . $dates . '</div><div class="footer-right">Downloads: ' . $clicks . '</div></div>';
-            echo '</div>';
-            $counter++;
-        }
+$sql  = "SELECT * FROM " . $DBtable . " ORDER BY dates DESC";
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+//set counter variable
+$counter = 1;
+if ($stmt->rowCount() > 0) {
+    $result = $stmt->fetchAll();
+    foreach ($result as $row) {
+        $rating     = $row['rating'];
+        $votes      = $row['votes'];
+        $avg        = ($rating == 0) ? 0 : round(($rating / $votes), 1);
+        $totalVotes = ($votes == 1) ? "stemme" : "stemmer";
+        $id         = $row['id'];
+        $title      = $row['title'];
+        $desc       = $row['description'];
+        $content    = $row['content'];
+        $url        = $row['url'];
+        $clicks     = number_format($row['clicks'], 0, '', '.');
+        $dates      = (date('d.m.Y', strtotime($row['dates'])));
+        echo '<div class="card">';
+        echo '<h4 class="card-header"><span class="count">' . $counter . '.</span> <a href="includes/downloads/downloads.php?id=' . $id . '" target="_blank" rel="noopener" title="Se ' . $title . '">' . $title . '</a></h4>';
+        echo '<div class="card-block"><p class="card-text">' . $desc . '</p>';
+        // Rating start
+        // echo '<span class="ratingAverage" data-av="' . $avg . '"></span>
+        //     <span class="item" data-id="' . $id . '"></span>';
+        // echo '<div class="bar">
+        //       <span class="bg"></span>
+        //       <span class="stars">';
+        // for ($i = 1; $i <= 5; $i++):
+        //     echo '<span class="star" data-vote="' . $i . '">
+        //         <span class="starimg"></span>
+        //       </span>';
+        // endfor;
+        // echo '</span></div><div class="votes"><span>' . $votes . ' ' . $totalVotes . '</span></div>';
+        // Rating end
+        echo '</div>';
+        echo '<div class="card-footer"><div class="footer-left">Oprettet: ' . $dates . '</div><div class="footer-right">Downloads: ' . $clicks . '</div></div>';
+        echo '</div>';
+        $counter++;
     }
-    else {
-        echo '<div class="alert alert-warning" role="alert">Ingen resultater fundet.</div>';
-    }
+} else {
+    echo '<div class="alert alert-warning" role="alert">Ingen resultater fundet.</div>';
+}
 
-  // Closing
-  $stmt = null;
-  $pdo = null;
+// Closing
+$stmt = null;
+$pdo  = null;
 //}
 ?>
--- src/js/custom.js
+++ src/js/custom.js
@@ -6,13 +6,13 @@
 var custom = function($) {
   $(document).ready(function() {
     // Variables to find paths for localhost and web
-    var $current_url = window.location.href;
-    var $isLocalhost = ($current_url.search('localhost') != -1)
-      ? true
-      : false;
-    var $path = ($isLocalhost === true)
-      ? 'http://localhost:8080/personal-website/'
-      : $current_url;
+    // var $current_url = window.location.href;
+    // var $isLocalhost = ($current_url.search('localhost') != -1)
+    //   ? true
+    //   : false;
+    // var $path = ($isLocalhost === true)
+    //   ? 'http://localhost:8080/personal-website/'
+    //   : $current_url;
 
     // Search
     var search = $('#search');
@@ -50,6 +50,47 @@ var custom = function($) {
       }, 500);
       searchDb();
     }
+
+    // Rating
+    // var average = $('.ratingAverage').attr('data-av');
+    // function getAvg(average) {
+    //   average = (Number(average) * 20);
+    //   $('.bg').css('width', 0);
+    //   $('.bar .bg').animate({
+    //     width: average + '%'
+    //   }, 500);
+    // }
+    //
+    // getAvg(average);
+    //
+    // $('.star').on('mouseover', function() {
+    //   var indexAtual = $('.star').index(this);
+    //   for (var i = 0; i <= indexAtual; i++) {
+    //     $('.star:eq(' + i + ')').addClass('full');
+    //   }
+    // });
+    //
+    // $('.star').on('mouseout', function() {
+    //   $('.star').removeClass('full');
+    // });
+    //
+    // $('.star').on('click', function() {
+    //   var itemId = $('.item').attr('data-id');
+    //   var vote = $(this).attr('data-vote');
+    //   $.post('/includes/rating/sys/vote.php', {
+    //     vote: 'yes',
+    //     item: itemId,
+    //     point: vote
+    //   }, function(data, status) {
+    //     //console.log(data);
+    //     //console.log(status);
+    //     getAvg(data.average);
+    //     var suffix = (data.votes == 1)
+    //       ? "stemme"
+    //       : "stemmer";
+    //     $('.votes span').html(data.votes + " " + suffix);
+    //   }, 'jSON');
+    // });
 
     // Lazy load single image
     setTimeout(function() {
