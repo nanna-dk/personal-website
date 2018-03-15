76767256c0e5944a966b0b2510a5c2b3f698a1a3
Nanna Ellegaard <nanna@e-nanna.dk>
2018-03-15 15:53:23 +0100

    Fetch GitHub stats

--- src/js/gitHubStats.js
+++ src/js/gitHubStats.js
@@ -18,21 +18,33 @@ var getGitHubStats = function($) {
         url: url,
         dataType: "json",
         success: function(data) {
-           console.log(data);
-          $.each(data, function(k, v) {
-            var date = v.commit.committer.date;
-            var message = v.commit.message;
+          console.log(data);
+          var newData = data.reduce((acc, el) => {
+            var date = el.commit.committer.date;
+            var msg = el.commit.message;
+            var sha = el.sha;
+            console.log(sha);
+            var d = date.split('T')[0];
+            if (acc.hasOwnProperty(d))
+              acc[d].push(msg);
+            else
+              acc[d] = [msg];
+            return acc;
+          }, {})
+          Object.keys(newData).forEach(function(v, k) {
+            //console.log(newData);
+            var date = v;
+            var message = newData[v].join(", ");
             var t = new Date(date);
-            var day = ("0" + (t.getDate())).slice(-2);
-            var month = ("0" + (t.getMonth() + 1)).slice(-2);
+            var day = ("0" + (
+            t.getDate())).slice(-2);
+            var month = ("0" + (
+            t.getMonth() + 1)).slice(-2);
             var year = t.getFullYear();
-            var allDates = $.makeArray(date);
-            console.log(allDates);
-            var sha = v.sha;
-            gitHubStats.append('<li>' + day + '/' + month + '/' + year + ": <a href=" + gitHubUrl + sha + " target='_blank' rel='noopener'>" + message + '</li>');
-            return k < maxHits;
+
+            gitHubStats.append('<li>' + day + '/' + month + '/' + year + ': <a href=' + gitHubUrl + ' target="_blank" rel="noopener">' + message + '</a></li>');
           });
-          gitHubStats.children().wrapAll( "<ul class='list-unstyled' />");
+          gitHubStats.children().wrapAll("<ul class='list-unstyled' />");
         },
         error: function(xhr, status, error) {
           console.log(xhr.responseText);
@@ -40,5 +52,6 @@ var getGitHubStats = function($) {
       });
     }
     fetchDatafromGitHub();
+
   });
 }(jQuery);
