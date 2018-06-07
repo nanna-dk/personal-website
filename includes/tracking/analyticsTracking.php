2db10e3f118708e3a9e6f0fc72cf1d4dccee2e88
Nanna Ellegaard <nanna@e-nanna.dk>
2018-06-07 13:46:38 +0200

    Gtag

--- includes/tracking/analyticsTracking.php
+++ includes/tracking/analyticsTracking.php
@@ -1,7 +1,11 @@
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-11508156-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-11508156-1');
-</script>
+<?php
+echo "
+  <script async src='https://www.googletagmanager.com/gtag/js?id=UA-11508156-1'></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-11508156-1');
+  </script>
+";
+?>
