32e44e7af4dd623f9e16b2a932ce7a450bcde45a
Nanna Ellegaard <nanna@e-nanna.dk>
2018-08-01 14:15:41 +0200

    Global functions

--- includes/functions.php
+++ includes/functions.php
@@ -22,7 +22,6 @@ function randomString($length) {
     $keys = array_merge(range(0,9), range('a', 'z'));
     $key = "";
     for($i=0; $i < $length; $i++) {
-        $key .= $keys[mt_rand(0, count($keys) - 1)];
     }
     return $key;
 }
