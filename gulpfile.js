54a23d5a330bb5dec1a6555da6e49c39de4a4e1a
Nanna Ellegaard <nanna@e-nanna.dk>
2018-08-17 11:57:29 +0200

    Updated package options

--- gulpfile.js
+++ gulpfile.js
@@ -21,16 +21,9 @@ var sassOptions = {
 // Autoprefixer options
 var autoprefixerOptions = {
   browsers: [
-    "last 1 major version",
-    ">= 1%",
-    "Chrome >= 45",
-    "Firefox >= 38",
-    "Edge >= 12",
-    "Explorer >= 10",
-    "iOS >= 9",
-    "Safari >= 9",
-    "Android >= 4.4",
-    "Opera >= 30"
+    "last 1 version",
+    "> 1%",
+    "not dead"
   ],
   cascade: false
 };
