faaec8bcb95f1abcd606ea1a119df1a0ded596d3
Nanna Ellegaard <nanna@e-nanna.dk>
2018-08-29 13:48:48 +0200

    Updated Service worker

--- sw.js
+++ sw.js
@@ -1,40 +1,25 @@
-/*
- *
- *  Nanna Ellegaard
- *  Copyright 2015 Google Inc. All rights reserved.
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License
- *
- */
+// Service worker
 
-const version = "0.1.0";
-const cacheName = `NEL-${version}`;
+const version = '0.1.0';
+const cacheName = 'NEL-${version}';
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => {
-      return cache.addAll([
-        '/',
-        'index.php',
-        'dist/css/bootstrap.min.css',
-        'dist/js/bootstrap.min.js',
-        'dist/js/svgxuse.min',
-        'img/it-cover.jpg',
-        'img/icons.svg',
-        'favicon.ico'
-      ])
-          .then(() => self.skipWaiting());
-    })
-  );
+  e.waitUntil(caches.open(cacheName).then(cache => {
+    return cache.addAll([
+      '/',
+      'index.php',
+      'dist/css/bootstrap.min.css',
+      'dist/js/bootstrap.min.js',
+      'dist/js/svgxuse.min.js',
+      'img/icons/favicon-16x16.png',
+      'img/icons/favicon-32x32.png',
+      'img/icons/android-chrome-192x192.png',
+      'img/icons/android-chrome-256x256.png',
+      'img/icons/android-chrome-512x512.png',
+      'img/it-cover.jpg',
+      'img/icons.svg',
+      'favicon.ico'
+    ]).then(() => self.skipWaiting());
+  }));
 });
 
 self.addEventListener('activate', event => {
@@ -42,11 +27,7 @@ self.addEventListener('activate', event => {
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.open(cacheName)
-      .then(cache => cache.match(event.request, {ignoreSearch: true}))
-      .then(response => {
-      return response || fetch(event.request);
-    })
-  );
+  event.respondWith(caches.open(cacheName).then(cache => cache.match(event.request, {ignoreSearch: true})).then(response => {
+    return response || fetch(event.request);
+  }));
 });
