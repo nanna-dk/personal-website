0988a01fb2e157608683b70abbf866f628de36c2
nanna_dk <nanna@e-nanna.dk>
2018-09-09 13:01:07 +0200

    Global functions

--- includes/functions.php
+++ includes/functions.php
@@ -5,14 +5,14 @@
 
 // Print out server name in a safe way
 function siteUrl() {
-  $base_url =  "https://www.{$_SERVER['SERVER_NAME']}";
+  $base_url =  "https://{$_SERVER['SERVER_NAME']}";
   $site = htmlspecialchars( $base_url, ENT_QUOTES, 'UTF-8' );
   return $site;
 }
 
 // Print out full site url in a safe way
 function fullSiteUrl() {
-  $base_url =  "https://www.{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+  $base_url =  "https://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
   $site = htmlspecialchars( $base_url, ENT_QUOTES, 'UTF-8' );
   return $site;
 }
