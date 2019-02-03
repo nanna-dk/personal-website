b23494080d5f8c9163788770c4facd0c517d2a49
nanna_dk <nanna@e-nanna.dk>
2019-02-03 11:06:15 +0100

    Added html5 download attribute

--- includes/tpl/assignment.php
+++ includes/tpl/assignment.php
@@ -24,8 +24,10 @@
   $url        = $row['url'];
   $clicks     = number_format($row['clicks'], 0, '', '.');
   $dates      = (date('d. m. Y', strtotime($row['dates'])));
+  // Urls containing zip archives require the download attribute
+  $dl_tag     = ($id == 2 || $id == 7) ? 'download' : '';
   echo '<div class="card">';
-  echo '<h4 class="card-header"><span class="count">' . $counter . '.</span> <a href="includes/downloads/downloads.php?id=' . $id . '" target="_blank" rel="noopener" title="Se ' . $title . '">' . $title . '</a></h4>';
+  echo '<h4 class="card-header"><span class="count">' . $counter . '.</span> <a href="includes/downloads/downloads.php?id=' . $id . '" target="_blank" rel="noopener" title="Se ' . $title . '" ' . $dl_tag .'>' . $title . '</a></h4>';
   echo '<div class="card-body"><p class="card-text">' . $desc . '</p>';
   // Rating start
   echo '<div class="ratings" data-id="' . $id . '" data-avg="' . $avg . '">';
