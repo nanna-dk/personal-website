fb2d99b05656a943cc6631c76ae2979df814946c
nanna_dk <nanna@e-nanna.dk>
2020-10-12 15:44:23 +0200

    Login

--- admin.php
+++ admin.php
@@ -1,38 +1,66 @@
 <!DOCTYPE html><html class="no-js" lang="da"><head> <?php include('includes/functions.php'); ?> <meta charset="utf-8"><meta http-equiv="x-ua-compatible" content="ie=edge"><meta name="robots" content="noindex"><meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport"><meta content="Administrator - Statistik og oversigt" name="description"><meta property="og:type" content="website"><meta property="og:title" content="Administrator-login"><meta property="og:image" content="<?php echo siteUrl(); ?>/img/icons/social-article-img.png"><meta property="og:image:width" content="1200"><meta property="og:image:height" content="627"><meta property="og:description" content="Administrator - Statistik og oversigt"><meta property="og:url" content="<?php echo siteUrl(); ?>"><link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png"><link rel="icon" type="image/png" sizes="32x32" href="img/icons/favicon-32x32.png"><link rel="icon" type="image/png" sizes="16x16" href="img/icons/favicon-16x16.png"><link rel="shortcut icon" href="favicon.ico"><link rel="manifest" href="manifest.json"><link rel="mask-icon" href="img/icons/safari-pinned-tab.svg" color="#d75959"><meta name="msapplication-TileColor" content="#d75959"><meta name="theme-color" content="#d75959"><title>Administrator-login</title> <?php include('includes/db.php'); ?> <link href="dist/css/style.min.css?v=<?php echo $version_number;?>" rel="stylesheet"> <?php include_once('includes/tracking/analyticsTracking.php'); ?> <script type="application/ld+json">{"@context":"http://schema.org","@type":"WebSite","url":"<?php echo siteUrl(); ?>","name":"Nanna Ellegard, Cand.it i Multimedier","description":"Administrator - Statistik og oversigt","publisher":"Nanna Ellegaard","potentialAction":{"@type":"SearchAction","target":"<?php echo siteUrl(); ?>?q={search_term_string}","query-input":"required name=search_term_string"}}</script><script type="application/ld+json">{"@context":"http://schema.org","@type":"Person","address":{"@type":"PostalAddress","addressLocality":"Copenhagen, Denmark"},"image":"<?php echo siteUrl(); ?>/img/nanna_ellegaard.jpg","jobTitle":"Front end developer","name":"Nanna Ellegaard","gender":"female","nationality":"Danish","owns":{"@type":"Website","name":"https://www.studenterforum.dk"},"worksFor":{"@type":"EducationalOrganization","name":"University of Copenhagen, Denmark"},"url":"<?php echo siteUrl(); ?>","sameAs":["https://www.facebook.com/nanna.ellegaard","https://www.linkedin.com/in/nannaellegaard/","https://plus.google.com/102076494911232201521?rel=author","https://github.com/nanna-dk/"]}</script></head><body><nav aria-label="Spring til indhold på siden" class="jumpbox" title="Spring til indhold på siden"><ul class="jump"><li><a accesskey="1" class="btn btn-primary btn-small" href="#global-nav" title="Spring til sidens menu">Emnenavigation</a></li><li><a accesskey="2" class="btn btn-primary btn-small" href="#about" title="Spring til indhold på siden">Indhold</a></li><li><a accesskey="3" class="btn btn-primary btn-small" href="#footer" title="Spring til footer">Footerlinks</a></li></ul></nav> <?php include('includes/downloads/displayInfo.php'); ?> <div class="progress scrollProgress"><div id="scrollProgress" class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div></div><nav class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark" id="global-nav" aria-label="Global navigation" itemscope itemtype="https://schema.org/SiteNavigationElement"><div class="container"><a class="navbar-brand mr-auto mr-lg-3" aria-label="Forside" href="/"><svg role="presentation"><use xlink:href="dist/img/icons.svg#icon-code"></use></svg> </a><button class="navbar-toggler p-0 border-0" type="button" data-toggle="offcanvas"><span class="sr-only">Toggle mobile menu</span> <span class="navbar-toggler-icon"></span></button><div class="navbar-collapse offcanvas-collapse"><ul class="navbar-nav mr-auto"><li class="nav-item"><a aria-label="Forside" class="nav-link" itemprop="url" href="/#about">Start <span class="sr-only">home</span></a></li><li class="nav-item"><a aria-label="Nannas CV" class="nav-link" itemprop="url" href="/#cv">CV</a></li><li class="nav-item"><a aria-label="Se opgaver" class="nav-link" itemprop="url" href="/#assignments">Opgaver</a></li><li class="nav-item"><a aria-label="Kontaktformular" class="nav-link" href="" itemprop="url" data-toggle="modal" data-target="#contactFormModal">Kontakt</a></li></ul><div class="utility-icons"><ul class="navbar-nav" itemscope itemtype="https://schema.org/Person"><li class="nav-item"><a aria-label="Facebook" class="nav-link" href="https://www.facebook.com/nanna.ellegaard" target="_blank" rel="noopener" itemprop="sameAs"><svg role="presentation"><use xlink:href="dist/img/icons.svg#icon-facebook"></use></svg></a></li><li class="nav-item"><a aria-label="LinkedIn" class="nav-link" href="https://www.linkedin.com/in/nannaellegaard/" target="_blank" rel="noopener" itemprop="sameAs"><svg role="presentation"><use xlink:href="dist/img/icons.svg#icon-linkedin"></use></svg></a></li><li class="nav-item"><a aria-label="send besked via kontaktformular" class="nav-link" href="" data-toggle="modal" data-target="#contactFormModal" itemprop="url"><svg role="presentation"><use xlink:href="dist/img/icons.svg#icon-mail"></use></svg></a></li><li class="nav-item"><a aria-label="GitHub" class="nav-link" href="https://github.com/nanna-dk/" target="_blank" rel="noopener" itemprop="sameAs"><svg role="presentation"><use xlink:href="dist/img/icons.svg#icon-github"></use></svg></a></li><li class="nav-item"><a aria-label="Skriv via Skype" class="nav-link" href="skype:nanna_dk?chat" rel="noopener" itemprop="sameAs"><svg role="presentation"><use xlink:href="dist/img/icons.svg#icon-skype"></use></svg></a></li><li class="nav-item"><a title="RSS" class="nav-link" href="includes/rss/" rel="noopener" target="_blank" itemprop="url"><svg role="presentation"><use xlink:href="dist/img/icons.svg#icon-rss"></use></svg></a></li></ul></div></div></div></nav><div class="content"> <?php
 error_reporting(E_ALL);
-include(realpath(__DIR__ . '/includes/db.php'));
+include realpath(__DIR__.'/includes/db.php');
+
 if (isset($_POST['submit'])) {
-    $user = filter_var($_POST["username"], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_LOW);
-    $psw  = filter_var($_POST["password"], FILTER_SANITIZE_STRING, FILTER_FLAG_STRIP_HIGH | FILTER_FLAG_STRIP_LOW);
+    //Retrieve the field values from our login form.
+    $username = ! empty($_POST['username']) ? trim($_POST['username']) : null;
+    $passwordAttempt = ! empty($_POST['password']) ? trim($_POST['password']) : null;
 
-    $sql = "SELECT * FROM " . $userTable . " WHERE username = :username LIMIT 1";
+    //Retrieve the user account information for the given username.
+    $sql = 'SELECT id, username, password FROM users WHERE username = :username';
     $stmt = $pdo->prepare($sql);
-    $stmt->bindValue(':username', $user, PDO::PARAM_STR);
+    $stmt->bindValue(':username', $username);
     $stmt->execute();
+    $user = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    if ($stmt->rowCount() > 0) {
-        $result = $stmt->fetchAll();
-        foreach ($result as $row) {
-            $db_username = $row['username'];
-            $db_password = $row['password'];
-        }
-        if (isset($db_username) && $user == $db_username && isset($db_password) && $psw == password_verify($psw, $db_password)) {
-            // PASSWORD PROTECTED AREA STARTS HERE:
-?> <section class="assignments" id="stats"><div class="container"><div class="card"><div class="card-body"><h5 class="card-title">Statistik</h5> <?php
-                include('includes/stats/stats.php');
-                ?> </div><div class="card-body"><div id="coords"><h5 class="card-title">Position</h5><div class="input-group mb-3"><div class="input-group-prepend"><button class="btn btn-secondary" type="button" id="generateCoords">Start</button></div><label for="generateCoords" class="sr-only">Find koordinater</label> <input id="geo" type="text" class="form-control" placeholder="Find koordinater" aria-label="Get coordiantes"></div></div></div><div class="card-body"><h5 class="card-title">Dev stats for dette website</h5><div id="gitHubStats"></div></div></div></div></section> <?php
-            // PASSWORD PROTECTED AREA ENDS
-        } else {
-            echo '<div class="container p-3"><div class="alert alert-danger" role="alert">Brugernavn og/eller adgangskode er ikke korrekt.</div></div>';
-        }
+    //If $row is FALSE.
+    if (false === $user) {
+        //Could not find a user with that username!
+        //PS: You might want to handle this error in a more user-friendly manner!
+        die('Incorrect username / password combination!');
     } else {
-        echo '<div class="container p-3"><div class="alert alert-danger" role="alert">Brugernavnet findes ikke - prøv igen.</div></div>';
+        //User account found. Check to see if the given password matches the
+        //password hash that we stored in our users table.
+        $validPassword = password_verify($passwordAttempt, $user['password']);
+
+        //If $validPassword is TRUE, the login has been successful.
+        if ($validPassword) {
+            include('admin-content.php');
+            //exit;
+        } else {
+            //$validPassword was FALSE. Passwords do not match.
+            die('Incorrect username / password combination!');
+        }
     }
 } else {
-    // IF THE FORM WAS NOT SUBMITTED - SHOW FORM:
-?> <div class="container px-3"><div class="card"><div class="card-body"><h5 class="card-title">Indtast login</h5><form method="post" class="mt-sm-4"><div class="form-group row"><label for="username" class="col-sm-3 col-form-label">Brugernavn</label><div class="col-sm-9"><input type="text" class="form-control" id="username" name="username" placeholder="Brugernavn" autofocus required></div></div><div class="form-group row"><label for="password" class="col-sm-3 col-form-label">Adgangskode</label><div class="col-sm-9"><input type="password" class="form-control" id="password" name="password" placeholder="Password" required></div></div><div class="form-group row"><div class="col-sm-9"><button type="submit" name="submit" class="btn btn-primary mb-2">Send</button></div></div></form></div></div></div> <?php
-  // Closing
-  $stmt = null;
-  $pdo  = null;
+echo '<div class="container px-3">', PHP_EOL,
+    '<div class="card">', PHP_EOL,
+      '<div class="card-body">', PHP_EOL,
+      '<h5 class="card-title">Indtast login</h5>', PHP_EOL,
+        '<form action="" method="post" class="mt-sm-4">', PHP_EOL,
+          '<div class="form-group row">', PHP_EOL,
+            '<label for="username" class="col-sm-3 col-form-label">Brugernavn</label>', PHP_EOL,
+            '<div class="col-sm-9">', PHP_EOL,
+              '<input type="text" class="form-control" id="username" name="username" placeholder="Brugernavn" autofocus required>', PHP_EOL,
+            '</div>', PHP_EOL,
+          '</div>', PHP_EOL,
+          '<div class="form-group row">', PHP_EOL,
+            '<label for="password" class="col-sm-3 col-form-label">Adgangskode</label>', PHP_EOL,
+            '<div class="col-sm-9">', PHP_EOL,
+              '<input type="password" class="form-control" id="password" name="password" placeholder="Password" required>', PHP_EOL,
+            '</div>', PHP_EOL,
+          '</div>', PHP_EOL,
+          '<div class="form-group row">', PHP_EOL,
+            '<div class="col-sm-9">', PHP_EOL,
+              '<button type="submit" name="submit" class="btn btn-primary mb-2">Send</button>', PHP_EOL,
+            '</div>', PHP_EOL,
+          '</div>', PHP_EOL,
+        '</form>', PHP_EOL,
+      '</div>', PHP_EOL,
+    '</div>', PHP_EOL,
+  '</div>', PHP_EOL;
 }
+
+
 ?> </div><div class="modal fade" id="contactFormModal" role="dialog" aria-labelledby="responseLabel" aria-hidden="true"><div class="modal-dialog"><div class="modal-content"><form id="contactform" method="POST"><div class="modal-header"><h5 class="modal-title" id="responseLabel">Kontakt</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><div class="form-group"><input type="text" name="name" class="form-control" id="name" aria-label="Indtast navn" placeholder="Navn" autofocus required></div><div class="form-group"><input type="email" name="email" class="form-control" id="email" aria-label="Indtast e-mailadresse" placeholder="E-mail" required></div><div class="form-group"><textarea rows="6" name="message" class="form-control" id="msg" aria-label="Skriv besked" placeholder="Besked"></textarea></div><div class="feedback" role="alert" aria-atomic="true"></div><input type="hidden" value="" id="g-recaptcha-response" name="g-recaptcha-response"><div class="captcha-box"><div class="g-recaptcha" id="recaptcha" data-callback="" aria-label="Captcha" data-theme="light"></div></div></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal">Luk</button> <button type="submit" class="btn btn-primary" id="sendMail">Send</button></div></form></div></div></div><div class="modal fade" id="cd-cover" tabindex="-1" role="dialog" aria-labelledby="coverTitle" aria-hidden="true"><div class="modal-dialog" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="coverTitle">Cover til CD-rom</h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body"><picture itemprop="image" itemscope itemtype="https://schema.org/ImageObject"><source itemprop="url contentUrl" srcset="img/it-cover.webp" type="image/webp"><source itemprop="url contentUrl" srcset="img/it-cover.jpg" type="image/jpeg"><img src="img/it-cover.jpg" alt="Cover" width="600" height="450" loading="lazy" class="img-fluid"><meta itemprop="width" content="600"><meta itemprop="height" content="450"></picture></div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal" aria-label="Close">Luk</button></div></div></div></div><div class="modal fade" id="animation" tabindex="-1" role="dialog" aria-labelledby="animationTitle" aria-hidden="true"><div class="modal-dialog modal-lg" role="document"><div class="modal-content"><div class="modal-header"><h5 class="modal-title" id="animationTitle">3D-modellering: Return of the Mac. <small class="text-muted">Mp4 m. lyd</small></h5><button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button></div><div class="modal-body text-center"><div class="embed-responsive embed-responsive-16by9 video-wrapper" itemprop="video" itemscope itemtype="https://schema.org/VideoObject"><video class="embed-responsive-item video-style" poster="img/3d-modellering.jpg" controls playsinline><source itemprop="contentUrl" src="includes/downloads/downloads.php?id=2">I’m sorry, your browser doesn’t support HTML5 video.</video><meta itemprop="name" content="3D modellering"><meta itemprop="description" content="Return of the Mac - en animation."><meta itemprop="duration" content="PT1M44S"><meta itemprop="uploadDate" content="2003-01-17T15:16:09+01:00"><meta itemprop="thumbnailUrl" content="img/3d-modellering.jpg"></div>Copyright © 2003 Nanna Ellegaard og Allan Lysholt</div><div class="modal-footer"><button type="button" class="btn btn-secondary" data-dismiss="modal" aria-label="Close">Luk</button></div></div></div></div><footer id="footer" class="site-footer"><div class="container"><div class="row"><div class="col-xs-12 col-md-6"><ul class="list-inline footer-links"><li class="list-inline-item"><a href="index.php#about" title="Naviger til forside">Forside</a></li><li class="list-inline-item"><a href="index.php#cv">CV</a></li><li class="list-inline-item"><a href="index.php#assignments" title="Find opgaver">Opgaver</a></li><li class="list-inline-item"><a href="" data-toggle="modal" data-target="#contactFormModal" title="Åbn kontaktformular">Kontakt</a></li></ul></div><div class="col-xs-12 col-md-6 text-lg-right text-xs-center mt-3 mt-sm-1"><p class="copyright-text">Sidst opdateret: <time datetime="<?php echo date(" Y-m-d " , getlastmod()) ?>"> <?php echo strftime( "%d. %m. %Y", getlastmod() ); ?></time><br>Copyright © <?php echo date("Y"); ?> <span itemprop="author" itemscope="itemscope" itemtype="https://schema.org/Person"><a href="admin.php" rel="author" itemprop="url"><span itemprop="name">Nanna Ellegaard</span></a></span></p></div></div></div></footer><nav aria-label="Scroll til toppen"><a class="scrolltop fade" title="Til top" href="#about"><svg role="presentation"><use xlink:href="dist/img/icons.svg#icon-angle-up"></use></svg></a></nav><script src="dist/js/script.min.js?v=<?php echo $version_number;?>"></script><script src="dist/js/svgxuse.min.js" defer="defer"></script></body></html>
